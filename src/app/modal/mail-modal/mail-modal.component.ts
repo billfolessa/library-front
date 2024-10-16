c9072cd57d95936ee0e14cb0df6e907ab2b040e8
billfolessa <billfolassi@gmail.com>
2024-10-16 08:54:34 -0400

    component mail

---
+++ src/app/modal/mail-modal/mail-modal.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mail-modal',
+  standalone: true,
+  imports: [],
+  templateUrl: './mail-modal.component.html',
+  styleUrl: './mail-modal.component.less'
+})
+export class MailModalComponent {
+
+}
