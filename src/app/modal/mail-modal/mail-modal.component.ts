4ca2ff32c8e3d0d74ce9f91307d6f4807e0b4128
billfolessa <billfolassi@gmail.com>
2024-10-17 09:01:52 -0400

    Init mail-modal component

--- src/app/modal/mail-modal/mail-modal.component.ts
+++ src/app/modal/mail-modal/mail-modal.component.ts
@@ -7,6 +7,19 @@ import { Component } from '@angular/core';
   templateUrl: './mail-modal.component.html',
   styleUrl: './mail-modal.component.less'
 })
-export class MailModalComponent {
+export class MailModalComponent implements OnInit {
 
+   @Input('customerId') customerId: number;
+
+  @Output() unDisplayModal = new EventEmitter();
+
+  public emailSubject : string = '';
+  public emailContent: string = '';
+
+  constructor(private loanService: LoanService, private bookService: BookService, 
+    private customerService: CustomerService, private http: HttpClient, private spinner: NgxSpinnerService) { }
+
+  ngOnInit() {
+  }
+   
 }
